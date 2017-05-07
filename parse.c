fbf876f976a9a762c11ae82f4a836f927658b684
Sean Brody <ifonefox@users.noreply.github.com>
2017-05-06 20:05:49 -0400

    Replace old String type with char array

--- parse.c
+++ parse.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "parse.h"
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   FILE *ptr;
-  char buffer[20];
-  int version;
-  int len;
   CollectionDB db;
   ptr = fopen("collection.db", "rb");
   parse_collectiondb(ptr, &db);
-  /*printf("%d\n",db.version);*/
-  /*printf("%d\n",db.len);*/
-  printf("l:%x\n",db.collections[0].len);
-
-
+  for (int i = 0; i < db.len; i++) {
+    printf("final name:%s\n",db.collections[i].name);
+  }
   return 0;
 }
 int parse_collectiondb(FILE *ptr, CollectionDB *db){
@@ -31,56 +25,56 @@ int parse_collectiondb(FILE *ptr, CollectionDB *db){
   bytes += sizeof(int);
   db->len = len;
   col = malloc(sizeof(Collection) * len);
-  parse_collection(ptr,&(col[0]));
+  db->collections = col;
+  for (int i = 0; i < len; i++) {
+    parse_collection(ptr,col+i); //address of col[i]
+  }
   return bytes;
 }
 int parse_collection(FILE *ptr, Collection *col){
   int bytes = 0;
   unsigned int len = 0;
   char temp;
-  int i;
-  String *name;
-  String **beatmaps;
+  unsigned int i;
+  char* name;
+  char** beatmaps;
   name = malloc(sizeof(String));
-  bytes += parse_string(ptr,name);
-  for(i = 0; i < name->len; i++){
-    printf("%x ",name->text[i]);
-  }
-  printf("\n");
+  bytes += parse_string(ptr,&name);
   col->name = name;
   // I am assuming you have less than 65536 beatmaps per collection
   // Please don't make me parse ULEB128
   for(i = 0; i < sizeof(unsigned int); i++){
     fread(&temp, sizeof(char), 1, ptr);
-    printf("temp:%x\n",temp);
     temp = temp << (8*i);
-    printf("temp2:%x\n",temp);
     len += (unsigned int)temp;
-    printf("len: %x\n",len);
   }
   bytes += sizeof(unsigned int);
-  printf("%u\n", len);
   col->len = len;
+  beatmaps = malloc(sizeof(char*)*len);
+  for (i = 0; i < len; i++) {
+    parse_string(ptr, &(beatmaps[i])); //address of beatmaps[i]
+  }
+  col->beatmaps = beatmaps;
   
   return bytes;
 }
-int parse_string(FILE *ptr, String *str){
+int parse_string(FILE *ptr, char** str){
   int bytes = 0;
   char type;
   char len;
-  int i;
   char *text;
   fread(&type, sizeof(char), 1, ptr);
   bytes++;
   if(type == 0x00){
+    *str = NULL;
     return bytes;
   }
   fread(&len, sizeof(char), 1, ptr);
   bytes++;
-  str->len = len;
-  text = malloc(sizeof(char)*len);
+  text = malloc(sizeof(char)*(len+1));
   fread(text, sizeof(char), len, ptr);
+  text[len] = '\0';
   bytes += len;
-  str->text = text;
+  *str = text;
   return bytes;
 }
